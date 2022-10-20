ad9c67300d7e20fcfa57f4afa5c954b0328cf212
jtmb <james.branco@gmail.com>
2022-10-20 19:27:55 -0400

    var changes

--- EC2/vars.tfvars
+++ EC2/vars.tfvars
@@ -1,3 +1,9 @@
+# ###########################################################################################################################################################
+# #                                                              !!!SET VARS HERE!!!                                                                      # #
+# #   Vars must be set in order for deploy to work. If you are unsure about what these VARS do, search for them in the project and get familiar.          # #
+#############################################################################################################################################################
+
+
 # Your AWS credentials file location
   # See here for more info: https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials_profiles.html
 credentials_file = ["/home/brajam/.aws/credentials"]
@@ -11,4 +17,4 @@ tags = {
     Environment = "PRD"
   }
 # Enter Pub Key Value to deploy to server, key must be in " "
-aws_key_pair = 
+aws_key_pair = ""
--- runner.sh
+++ runner.sh
@@ -3,10 +3,47 @@ echo
 echo Provisioning EC2 with Terraform
 echo
 echo -------------------------------
+
 cd EC2 && terraform apply -var-file=vars.tfvars
+
+echo
+echo
+echo
+echo
 echo -------------------------------
 echo
-echo Post Provision tasks with Ansible have kicked off
+echo Proceeding with Ansible Post Provision Play
+echo
+echo -------------------------------
+echo
+echo
+echo -------------------------------------------------------------------WAIT--------------------------------------------------------------------------------------
+echo -----------------------------------------------------!!!??DID YOU SET YOUR VARS??!!!-------------------------------------------------------------------------
+echo --------ANSIBLE Vars must be set in order for deploy to work. If you are unsure about what these VARS do or where they are :---------------------------------                             
+echo -------------------------------------------------------------------------------------------------------------------------------------------------------------
+echo --------------------------------------------The following ansible vars must be set in vars.yml:--------------------------------------------------------------
+echo ----------------------------------------"target_server_ip" "private_server_ip" "ssh_cert" "ssh_user:"--------------------------------------------------------
+echo -------------------------------------------------------------------------------------------------------------------------------------------------------------
+echo ---------------------Once you have changed the var values, simply save your changes and answer "yes" to the prompt below.------------------------------------
+echo -------------------------------------------------------------------------------------------------------------------------------------------------------------
+echo
+echo
+echo
+echo
+echo
+
+# Display Output confirmation request
+    while true; do
+        read -p "Do you have want to continue?" yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit ;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+echo -------------------------------
+echo
+echo Post Provision tasks with Ansible has kicked off
 echo 
 echo -------------------------------
 cd .. && cd post-provisioning && ansible-playbook -i inventory.ini main.yml
